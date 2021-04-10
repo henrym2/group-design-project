74eda46048c5b40eab05d98ad5dfc1d1c2fa48fd
henrym2 <henrym2@tcd.ie>
2021-04-10 12:43:35 +0100

    Updated profile Added skills, experience and others in profile Added section for removing reviews

--- src/pages/engineerProfile.vue
+++ src/pages/engineerProfile.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
         <v-row>
-            <v-card width="95rem" height="85vh" class="mt-3">
+            <v-card width="95rem" class="mt-3">
                 <v-toolbar
                     class="toolbar-color"
                     dark
@@ -19,25 +19,90 @@
                         <v-tab>
                             User Details
                         </v-tab>
-                        <v-tab>
-                            Your Projects
+                        <v-tab v-if="userData.comments">
+                            Your Reviews
                         </v-tab>
                     <v-tab-item>
                         <v-card flat>
                             <v-card-title>Details</v-card-title>
                             <v-card-text>
-                                <v-text-field label="Email"></v-text-field>
-                                <v-text-field label="Username"></v-text-field>
-                                <v-text-field label="Password"></v-text-field>
-                                <v-textarea label="About Me" clearable counter no-resize></v-textarea>
-
+                                <!-- User details -->
+                                <v-text-field label="Username" v-model="userData.name"></v-text-field>
+                                <v-chip-group>
+                                    <v-chip
+                                        v-for="skill in userData.skills"
+                                        :key="skill"
+                                        close
+                                        @click:close="removeItem('skills', skill)"
+                                    >
+                                        {{skill}}
+                                    </v-chip>
+                                </v-chip-group>
+                                <v-text-field 
+                                    label="New Skill"
+                                    append-icon="mdi-plus"
+                                    v-model="newData.skills"
+                                    @click:append="addNew('skills')"
+                                    @keydown.enter="addNew('skills')"
+                                ></v-text-field>
+                                <v-chip-group>
+                                    <v-chip
+                                        v-for="qualification in userData.qualifications"
+                                        :key="qualification"
+                                        close
+                                        @click:close="removeItem('qualifications', qualification)"
+                                    >
+                                        {{qualification}}
+                                    </v-chip>
+                                </v-chip-group>
+                                <v-text-field 
+                                    label="New qualifications"
+                                    append-icon="mdi-plus"
+                                    v-model="newData.qualifications"
+                                    @click:append="addNew('qualifications')"
+                                    @keydown.enter="addNew('qualifications')"
+                                ></v-text-field>
+                                <v-chip-group>
+                                    <v-chip
+                                        v-for="experience in userData.experience"
+                                        :key="experience"
+                                        close
+                                        @click:close="removeItem('experience', experience)"
+                                    >
+                                        {{experience}}
+                                    </v-chip>
+                                </v-chip-group>
+                                <v-text-field 
+                                    label="New experience"
+                                    append-icon="mdi-plus"
+                                    v-model="newData.experience"
+                                    @click:append="addNew('experience')"
+                                    @keydown.enter="addNew('experience')"
+                                ></v-text-field>
+                                <v-textarea label="About Me" clearable counter no-resize v-model="userData.about"></v-textarea>
+                                
                             </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn color="success" @click="saveAll">Save</v-btn>
+                            </v-card-actions>
                         </v-card>
                     </v-tab-item>
-                    <v-tab-item class="overflow-auto">
+                    <!-- User Comments/Reviews -->
+                    <v-tab-item v-if="userData.comments" class="overflow-auto">
                         <v-card flat>
-                            <v-card-title>User Projects</v-card-title>
+                            <v-card-title>User Reviews</v-card-title>
                             <v-card-text>
+                                <v-card v-for="comment in userData.comments" :key="comment.id">
+                                    <v-card-title>Review from {{comment.name}}</v-card-title>
+                                    <v-card-text>
+                                        {{comment.text}}
+                                    </v-card-text>
+                                    <v-card-actions>
+                                        <v-spacer/>
+                                        <v-btn color="red" @click="removeComment(comment.id)">Remove</v-btn>
+                                    </v-card-actions>
+                                </v-card>
                             </v-card-text>
                         </v-card>
                     </v-tab-item>
@@ -48,22 +113,70 @@
 </template>
 
 <script>
-
+import {db} from "../firebase/firebase.js"
 export default {
     data () {
         return {
-            tabs: [
-                "Page 1",
-                "Page 2"
-            ],
             tabbed: "",
+            userData: JSON.parse(sessionStorage.getItem('user')),
+            newData: {
+                skills: "",
+                experience: "",
+                qualifications: "",
+            }
         }
     },
-    computed: {
-        userData() {
-            return JSON.parse(sessionStorage.getItem('user'))
+    mounted: function() {
+        this.getUser()
+    },
+    methods: {
+        async getUser() {
+            try {
+                let user = await db.collection("users").doc(sessionStorage.getItem("userid")).get()
+                this.userData = user.data()
+                sessionStorage.setItem(JSON.stringify(this.userData))
+            } catch (e) {
+                console.log(e)
+            }
+        },
+        async removeComment (id) {
+            try {
+                this.userData.comments = this.userData.comments.filter(e => e.id !== id)
+                const userID = sessionStorage.getItem("userid")
+                await db.collection("users").doc(userID).set(this.userData, {merge: true})
+                sessionStorage.setItem("user", JSON.stringify(this.userData))
+            } catch (e) {
+                console.log(e)
+            }
+        },
+        async addNew(param) {
+            try {
+                this.userData[param].push(this.newData[param])
+                this.newData[param] = ""
+
+            } catch (e) {
+                console.log(e)
+            }
+        },
+        async removeItem(param, item) {
+            try {
+                this.userData[param] = this.userData[param]?.filter(e => e !== item)
+                this.newData[param] = ""
+
+            } catch (e) {
+                console.log(e)
+            }
+        },
+        async saveAll() {
+            try {
+                await db.collection("users").doc(sessionStorage.getItem("userid")).set(this.userData, {merge: true})
+                sessionStorage.setItem("user", JSON.stringify(this.userData))
+            } catch (e) {
+                console.log(e)
+            }
         }
     }
+    
 }
 </script>
 
